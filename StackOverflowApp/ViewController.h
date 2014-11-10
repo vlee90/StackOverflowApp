59fc32ae476c0bdd90c99b02d2bec244f33072d3
Vincent Lee <lee.m.vincent@gmail.com>
2014-11-10 12:28:42 -0800

    Initial Commit

---
+++ StackOverflowApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOverflowApp
+//
+//  Created by Vincent Lee on 11/10/14.
+//  Copyright (c) 2014 VincentLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StackOverflowApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StackOverflowApp
+//
+//  Created by Vincent Lee on 11/10/14.
+//  Copyright (c) 2014 VincentLee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
